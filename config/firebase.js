48b2ade3f9dc32fc25d823a0d58d75d7eade0082
CrzyF <69329270+CrzyF@users.noreply.github.com>
2024-06-24 18:41:12 +0000

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIQhmD66xCF3E5CsEOfhT50aqTh-EdAAc",
-  authDomain: "courtai.firebaseapp.com",
-  projectId: "courtai",
-  storageBucket: "courtai.appspot.com",
-  messagingSenderId: "139580026998",
-  appId: "1:139580026998:web:65556b7b6af3b64bd76f57",
-  measurementId: "G-7CSNGC76WB"
+  apiKey: ENTER_API_KEY,
+  authDomain: ENTER_authDomain,
+  projectId: ENTER_projectId,
+  storageBucket: ENTER_storageBucket,
+  messagingSenderId: ENTER_messagingSenderId, 
+  appId: ENTER_appId,
+  measurementId: ENTER_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
